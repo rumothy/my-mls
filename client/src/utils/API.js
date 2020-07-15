24891cebf4f616511bb9bc4867764c04efead92f
rumothy <rumothy@yahoo.com>
2020-07-15 09:01:55 -0400

    Add getProperties to API.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,5 +5,9 @@ import axios from "axios";
 export default {
   getRecipes: function(query) {
     return axios.get("/api/recipes", { params: { q: query } });
+  },
+
+  getProperties: function(query){
+    return axios.get("/api/properties", { params: { q: query} })  
   }
 };
